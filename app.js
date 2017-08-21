a366648f8cf1b075a092410a34a417b9b1af90f8
Marcin-Wiglusz <marcin.m.wiglusz@gmail.com>
2017-08-22 00:46:13 +0200

    adding bar movement

--- app.js
+++ app.js
@@ -9,10 +9,38 @@ var y = canvas.height - 100;
 // variables imitating ball movement, added in draw function to x and y
 var dx = 2; // increase value and you'll increase ball speed 
 var dy = -2;
+
 var ballRadius = 10;
-var paddleHeight = 10;
-var paddleWidth = 75;
-var paddleX = (canvas.width - paddleWidth) / 2;
+var barHeight = 10;
+var barWidth = 75;
+var barX = (canvas.width - barWidth) / 2;
+
+var rightPressed = false;
+var leftPressed = false;
+
+
+//moving bar with right/left arrows
+
+document.addEventListener('keydown', keyDownHandler);
+document.addEventListener('keyup', keyUpHandler);
+
+function keyDownHandler(event) {
+  if (event.keyCode == 39){
+    rightPressed = true;
+  }
+  else if (event.keyCode == 37) {
+    leftPressed = true;
+  }
+}
+function keyUpHandler(event) {
+  if (event.keyCode == 39) {
+    rightPressed = false;
+  }
+  else if (event.keyCode == 37) {
+    leftPressed = false;
+  }
+}
+
 
 function drawBall() {
   ctx.beginPath();
@@ -22,9 +50,9 @@ function drawBall() {
   ctx.closePath();
 }
 
-function drawPaddle() {
+function drawBar() {
   ctx.beginPath();
-  ctx.rect(paddleX, canvas.height - 2 * paddleHeight, paddleWidth, paddleHeight);
+  ctx.rect(barX, canvas.height - 2 * barHeight, barWidth, barHeight);
   ctx.fillStyle = '#FF0000';
   ctx.fill();
   ctx.closePath();
@@ -34,16 +62,26 @@ function draw() {
   //clears ball trail in canvas
   ctx.clearRect(0,0, canvas.width, canvas.height);
   
+  drawBall();
+  drawBar();
+  
   //making ball bounce off the canvas edge
   if (y + dy < ballRadius || y + dy > canvas.height - ballRadius) { //ballRadius val allows to bounce from the edge, withe the ball's edge
     dy = -dy;
   }
   if (x + dx < ballRadius || x + dx > canvas.width - ballRadius) {
     dx = -dx;
+  }  
+  
+  //moving the bar
+  if (rightPressed && barX < canvas.width - barWidth) {
+    barX += 6;
+  }
+  else if (leftPressed && barX > 0) {
+    barX -= 6;
   }
   
-  drawBall();
-  drawPaddle();
+
   
   x += dx;
   y += dy;    
